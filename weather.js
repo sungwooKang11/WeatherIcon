9adc430cc5392c66f9be12980f1e1489b9ecfb4c
sungwookang <sungwookang11@gmail.com>
2022-02-05 02:02:07 +0900

    done

--- weather.js
+++ weather.js
@@ -5,8 +5,8 @@ let weather = ["Clear", "Cloud", "Rain", "Snow", "Windy"];
 // function geoGood(position) {
 //     const lat = position.coords.latitude;
 //     const lon = position.coords.longitude;
-//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    
+//     const url = `https://api.openweathermap.org/data/2.5/weather?
+//     lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 //     fetch(url)
 //     .then(response => response.json())
 //     .then(data => {
